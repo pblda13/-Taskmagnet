1e763da82e6c6150fc54ffb8a7beab801fac9f30
Pamela <pamela_borges18@hotmail.com>
2024-05-06 12:44:28 -0300

    Model creation

--- src/main/java/com/projeto/taskmagnetdemo/model/Task.java
+++ src/main/java/com/projeto/taskmagnetdemo/model/Task.java
@@ -1,15 +1,36 @@
 package com.projeto.taskmagnetdemo.model;
 
 import com.projeto.taskmagnetdemo.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
-    private Date creationData;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Date deadline;
-   private Status status;
+
+    private Status status;
+
+    @PrePersist
+    protected void onCreate() {
+        creationDate = new Date();
+    }
 }
